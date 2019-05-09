2b848e89fb8f00894ad45ed1d8086cd5d8f04e87
Yusong Hsu <sky172839465@gmail.com>
2019-05-09 19:07:22 +0800

    refactor: add demo

--- easy/001-two-sum.js
+++ easy/001-two-sum.js
@@ -3,7 +3,7 @@
  * 2. 在 map 中找到 (target - 現在值) 的欄位就代表有符合兩數相加等於 target 的項目
  * 3. result 先放 map 的索引再放目前值的索引，因為 map 的索引一定比現在值早出現
  */
-
+const { demo } = require('../demoHelper')
 const nums = [2, 7, 11, 15]
 const target = 9
 const twoSum = (nums, target) => {
@@ -21,4 +21,4 @@ const twoSum = (nums, target) => {
   }
   return result
 }
-console.log(twoSum(nums, target))
+demo(twoSum, [nums, target], [0, 1])
