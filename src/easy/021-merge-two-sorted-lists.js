47ce03ab1486061f01038cd76a3c28d0a41d43b3
Yusong Hsu <sky172839465@gmail.com>
2019-05-13 03:26:38 +0800

    refactor: using spread syntax instead of concat

--- src/easy/021-merge-two-sorted-lists.js
+++ src/easy/021-merge-two-sorted-lists.js
@@ -39,7 +39,7 @@ const createListNode = list => {
   return listNode
 }
 const mergeTwoLists = (l1, l2) => {
-  const nodeValues = getFullNodeValues(l1).concat(getFullNodeValues(l2))
+  const nodeValues = [...getFullNodeValues(l1), ...getFullNodeValues(l2)]
   const sortNodeValues = nodeValues.sort((a, b) => a - b)
   return createListNode(sortNodeValues)
 }
