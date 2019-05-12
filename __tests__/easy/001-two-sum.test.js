42d4670cfe1cf70037f07732e8ea481b43269cac
Yusong Hsu <sky172839465@gmail.com>
2019-05-12 14:27:06 +0800

    test: 001 add new test case

--- __tests__/easy/001-two-sum.test.js
+++ __tests__/easy/001-two-sum.test.js
@@ -2,4 +2,6 @@ import twoSum from '../../src/easy/001-two-sum'
 
 test('twoSum test case', () => {
   expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1])
+  expect(twoSum([2, 7, 11, 15], 18)).toEqual([1, 2])
+  expect(twoSum([2, 7, 11, 15], 17)).toEqual([0, 3])
 })
