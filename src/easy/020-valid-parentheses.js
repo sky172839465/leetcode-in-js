e22b99cd94214e3dd361b3a0b469df155491c018
Yusong Hsu <sky172839465@gmail.com>
2019-05-12 17:58:44 +0800

    refactor: update regexp for replace all match string

--- src/easy/020-valid-parentheses.js
+++ src/easy/020-valid-parentheses.js
@@ -6,7 +6,7 @@
  * 5. 刪掉相連的括號直到輸入值清空代表這個輸入值有效
  * 6. 如果刪除後和刪除前字串一樣代表沒有一對的括號所以輸入值無效
  */
-const regexp = /({}|\[\]|\(\))/
+const regexp = /({}|\[\]|\(\))/g
 const isValid = s => {
   if (s.length === 0) {
     return true
