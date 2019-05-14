6008fcc63fcc7be1b49ed21a33095c430215226a
Yusong Hsu <sky172839465@gmail.com>
2019-05-14 13:02:27 +0800

    refactor: remove default cause coverage down

--- src/easy/027-remove-element.js
+++ src/easy/027-remove-element.js
@@ -3,7 +3,7 @@
  * 2. 從陣列尾端開始遞迴(不從0開始遞迴是為了避免刪除項目順序被打亂)
  * 3. 目前項目和 val 比較，一樣代表屬於要刪除的項目，刪除 nums 中這個 index 的項目
  */
-const removeElement = (nums = [], val) => {
+const removeElement = (nums, val) => {
   for (let i = (nums.length - 1); i > -1; i--) {
     if (nums[i] === val) {
       nums.splice(i, 1)
