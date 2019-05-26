aa20fe77b2f056fc061a5aa36650802ec2c666fe
Yusong Hsu <sky172839465@gmail.com>
2019-05-26 21:12:31 +0800

    fix: ignore wrong file

---
+++ src/easy/100-same-tree/index.js
@@ -0,0 +1,11 @@
+const isSameTree = (p, q) => {
+  if (!p && !q) {
+    return true
+  }
+  if (!p || !q || (p.val !== q.val)) {
+    return false
+  }
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+}
+
+export default isSameTree
