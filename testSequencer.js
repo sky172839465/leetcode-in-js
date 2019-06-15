b0c00f24407dc88317f0be0c2133ba036d768107
Yusong Hsu <sky172839465@gmail.com>
2019-06-15 20:12:30 +0800

    feat: unit test sort by file name

--- testSequencer.js
+++ testSequencer.js
@@ -1,9 +1,12 @@
 const Sequencer = require('@jest/test-sequencer').default
+const path = require('path')
 
 class CustomSequencer extends Sequencer {
   sort (tests) {
     const copyTests = Array.from(tests)
-    return copyTests.sort((testA, testB) => (testA.path > testB.path ? 1 : -1))
+    return copyTests.sort((testA, testB) => (
+      path.basename(testA.path) > path.basename(testB.path) ? 1 : -1)
+    )
   }
 }
 
